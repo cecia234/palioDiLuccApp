68526c8d7e2dbc55d1440854732d9625c78c2fc0
cecia234 <cecia234@gmail.com>
2023-09-13 11:04:58 +0200

    [feat] Add reviewer

--- components/home/setAchievementReviewerModal.tsx
+++ components/home/setAchievementReviewerModal.tsx
@@ -3,34 +3,42 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 import styles from './achievement.module.css';
-import SetAchievementReviewerModal from './setAchievementReviewerModal';
 
+interface IUser {
+    username: string,
+    propic: string
+}
 
+const reviewers: IUser[] = [
+    {
+        username: 'ciccio',
+        propic: '🧔🏻‍♂️'
+    },
+    {
+        username: 'mario',
+        propic: '👨🏻‍🦲'
+    },
+    {
+        username: 'giovanni',
+        propic: '👨🏻'
+    },
+]
 
-
-
-export default function AddAchievementModal(props) {
-    const [originalList, setOriginalList] = useState(props.achievementsToComplete);
+export default function SetAchievementReviewerModal(props) {
+    const [originalList, setOriginalList] = useState(reviewers);
     const [filteredList, setFilteredList] = useState(originalList);
     const [searchText, setSearchText] = useState('');
-    const [showAchievementReviewerModal, setShowAchievementReviewerModal] = useState(false);
-    const [selectedAchievement, setSelectedAchievement] = useState({ icon: '', name: '', description: '', status: 'undone' })
     const handleInputChange = (e) => {
         const newText = e.target.value;
         setSearchText(newText);
 
         // Filtra la lista originale in base al testo di input
         const filtered = originalList.filter((item) =>
-            item.name.toLowerCase().includes(newText.toLowerCase())
+            item.username.toLowerCase().includes(newText.toLowerCase())
         );
 
         setFilteredList(filtered);
     };
-
-    const setAchievementReviewer = (achievement) => {
-        setSelectedAchievement(achievement);
-        setShowAchievementReviewerModal(true)
-    }
 
     return (
         <>
@@ -40,20 +48,23 @@ export default function AddAchievementModal(props) {
             >
                 <Modal.Header closeButton className={styles.achievementModal}>
                     <Modal.Title>
-                        Aggiungi un achievement completato
+                        Aggiungi achievement: seleziona testimone
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className={styles.achievementModal}>
                     <div>
                         <input
                             type="text"
-                            placeholder="Cerca achievements..."
+                            placeholder="Cerca tra gli utenti..."
                             value={searchText}
                             onChange={handleInputChange}
                         />
                         <div className={styles.itemList}>
                             {filteredList.map((item) => (
-                                <div className={styles.achievementDiv} key={item.id} onClick={() => setAchievementReviewer(item)}><p>{item.icon} <b>{item.name}</b></p><p><i>{item.description}</i></p></div>
+                                <div className={styles.achievementDiv} key={item.username} onClick={() => {
+                                    props.onHide()
+                                    alert("TODO: invia richiesta reviewer")
+                                }}><p>{item.propic} <b>{item.username}</b></p></div>
                             ))}
                         </div>
                     </div>
@@ -62,14 +73,6 @@ export default function AddAchievementModal(props) {
                     <Button onClick={props.onHide} className={styles.modalBackButton}>Indietro</Button>
                 </Modal.Footer>
             </Modal>
-
-
-
-            <SetAchievementReviewerModal
-                show={showAchievementReviewerModal}
-                onHide={() => setShowAchievementReviewerModal(false)}
-                data={selectedAchievement}
-            />
         </>
     );
 }
--- components/home/viewAchievementModal.tsx
+++ components/home/viewAchievementModal.tsx
@@ -6,32 +6,32 @@ import styles from './achievement.module.css';
 
 export default function ViewAchievementModal(props) {
     return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton className={styles.achievementModal}>
-          <Modal.Title id="contained-modal-title-vcenter">
-            {props.data.name}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className={styles.achievementModal}>
-          <h2 className={styles.modalIcon}>
-            {props.data.icon}
-          </h2>
-          <p className={styles.modalDescription}>
-            {props.data.description}
-          </p>
-        </Modal.Body>
-        <Modal.Footer className={styles.achievementModal}>
-          <Button onClick={props.onHide} className={styles.modalBackButton}>Indietro</Button>
-          {
-            props.data.status==='undone' ?
-            <Button onClick={props.onHide} className={styles.modalCompleteButton}>Completa</Button> : null
-          }
-        </Modal.Footer>
-      </Modal>
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton className={styles.achievementModal}>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.data.name}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className={styles.achievementModal}>
+                <h2 className={styles.modalIcon}>
+                    {props.data.icon}
+                </h2>
+                <p className={styles.modalDescription}>
+                    {props.data.description}
+                </p>
+            </Modal.Body>
+            <Modal.Footer className={styles.achievementModal}>
+                <Button onClick={props.onHide} className={styles.modalBackButton}>Indietro</Button>
+                {
+                    props.data.status === 'undone' ?
+                        <Button onClick={props.onAdd} className={styles.modalCompleteButton}>Completa</Button> : null
+                }
+            </Modal.Footer>
+        </Modal>
     );
-  }
+}
