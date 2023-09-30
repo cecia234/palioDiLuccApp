373f08793fa05a28d28ed1d401d1f4a3220510a7
cecia234 <cecia234@gmail.com>
2023-09-30 17:23:59 +0200

    [fix] update schema and add default users population

---
+++ schema/user-insert-data.sql
@@ -0,0 +1,7 @@
+insert into public."user" values('marione', 'Mario', 'monteverdi', 'wario@a.it', '🥵');
+insert into public."user" values('scarredGio', 'Giovanni', 'monteverdi', 'scarredGio@a.it', '🧔🏻‍♂️');
+insert into public."user" values('marass', 'Mara', 'monteverdi', 'marass@a.it', '🧟‍♀️');
+insert into public."user" values('frass', 'Francesco', 'monteverdi', 'frass@a.it', '🥸');
+insert into public."user" values('cicc55', 'Francesco', 'monteverdi', 'cicc55@a.it', '🚀');
+insert into public."user" values('cecia234', 'Eugenio', 'monteverdi', 'cecia234@a.it', '🧑🏻‍💻');
+insert into public."user" values('nupas', 'Roberto', 'monteverdi', 'cecia234@a.it', '🤑');
