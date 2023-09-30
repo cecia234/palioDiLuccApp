e9fe2905f9ec54dad75214bb6c84d388f4c6c36b
cecia234 <cecia234@gmail.com>
2023-09-30 18:04:43 +0200

    [fix] fix default values

--- schema/achievement-insert-data.sql
+++ schema/achievement-insert-data.sql
@@ -1,9 +1,9 @@
 insert into public."achievement" values('JACKPOT!!!', 'Ottieni tutti gli achievements', '🤑', 3);
-insert into public."achievement" values('IL RITORNO DEL BROLYCULO', 'Sai già cosa devi fare', '🤑', 3);
-insert into public."achievement" values('GOURMET RACE', 'Mangia kebab (o falafel), ramen e pizza in una sola giornata', '🤑', 2);
-insert into public."achievement" values('LA CUMMINA', 'Compra e bevi una bevanda discutibile con cummine dentro', '🤑', 1);
-insert into public."achievement" values('ULTRA SIDE CHARACTER', 'Ma chi cazzo cosplaya Shino Aburame? Fai una foto con un personaggio super side', '🤑', 2);
-insert into public."achievement" values('PARITA’', 'Incontra e fai una foto con un content creator, ma di genere opposto al tuo.', '🤑', 1);
-insert into public."achievement" values('GAMER ESPERTO', 'Prova almeno 5 videogiochi nello stesso giorno. Punti bonus se partecipi a un torneo o a un evento videoludico.', '🤑', 3);
-insert into public."achievement" values('THE STORM THAT IS APPROACHING', 'Bevi una Monster Energy Zero Ultra (e sopravvivi per raccontarlo ai posteri)', '🤑', 1);
-insert into public."achievement" values('NANOMACHINES, SON!', 'Sperimenta la realtà virtuale', '🤑', 1);
+insert into public."achievement" values('IL RITORNO DEL BROLYCULO', 'Sai già cosa devi fare', '🍑', 3);
+insert into public."achievement" values('GOURMET RACE', 'Mangia kebab (o falafel), ramen e pizza in una sola giornata', '🧑🏻‍🍳', 2);
+insert into public."achievement" values('LA CUMMINA', 'Compra e bevi una bevanda discutibile con cummine dentro', '💦', 1);
+insert into public."achievement" values('ULTRA SIDE CHARACTER', 'Ma chi cazzo cosplaya Shino Aburame? Fai una foto con un personaggio super side', '❓', 2);
+insert into public."achievement" values('PARITA’', 'Incontra e fai una foto con un content creator, ma di genere opposto al tuo.', '🧬', 1);
+insert into public."achievement" values('GAMER ESPERTO', 'Prova almeno 5 videogiochi nello stesso giorno. Punti bonus se partecipi a un torneo o a un evento videoludico.', '🎮', 3);
+insert into public."achievement" values('THE STORM THAT IS APPROACHING', 'Bevi una Monster Energy Zero Ultra (e sopravvivi per raccontarlo ai posteri)', '🌌', 1);
+insert into public."achievement" values('NANOMACHINES, SON!', 'Sperimenta la realtà virtuale', '🥽', 1);
