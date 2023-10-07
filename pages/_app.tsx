5ad8d2e5a3b37174eaf41a4ff799701234f00e6f
cecia234 <cecia234@gmail.com>
2023-10-07 18:35:41 +0200

    [fix] add preloaded bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-  }
+  return <Component {...pageProps} />;
+}
