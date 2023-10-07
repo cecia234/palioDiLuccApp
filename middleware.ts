340dae4024c4af1ebc5ed9e995e4a938fc0dc691
cecia234 <cecia234@gmail.com>
2023-10-07 18:36:13 +0200

    [fix] better middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,22 @@
 import { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from './firebaseConfig';
 
 export function middleware(request: NextRequest) {
     // Call our authentication function to check the request
-    if (!isAuthenticated(request)) {
+    if (!request.nextUrl.pathname.startsWith('/login') && !isAuthenticated()) {
         // Respond with JSON indicating an error message
         return NextResponse.redirect(new URL('/login', request.url))
     }
+
+    return NextResponse.next();
 }
 
-function isAuthenticated(rquest: NextRequest) {
-    return true;
+function isAuthenticated() {
+    return onAuthStateChanged(auth, (user) => {
+        return !!user;
+    });
 }
+
+export const config = { matcher: '/((?!.*\\.).*)' }
