af5e13d37a3a42d9542faff9825f470882348f8a
Eugenio <cecia234@gmail.com>
2023-10-16 19:58:34 +0200

    [chore] new achievements

--- schema/achievement-insert-data.sql
+++ schema/achievement-insert-data.sql
@@ -1,9 +1,72 @@
-insert into public."achievement" values('JACKPOT!!!', 'Ottieni tutti gli achievements', '🤑', 3);
-insert into public."achievement" values('IL RITORNO DEL BROLYCULO', 'Sai già cosa devi fare', '🍑', 3);
-insert into public."achievement" values('GOURMET RACE', 'Mangia kebab (o falafel), ramen e pizza in una sola giornata', '🧑🏻‍🍳', 2);
-insert into public."achievement" values('LA CUMMINA', 'Compra e bevi una bevanda discutibile con cummine dentro', '💦', 1);
-insert into public."achievement" values('ULTRA SIDE CHARACTER', 'Ma chi cazzo cosplaya Shino Aburame? Fai una foto con un personaggio super side', '❓', 2);
-insert into public."achievement" values('PARITA’', 'Incontra e fai una foto con un content creator, ma di genere opposto al tuo.', '🧬', 1);
-insert into public."achievement" values('GAMER ESPERTO', 'Prova almeno 5 videogiochi nello stesso giorno. Punti bonus se partecipi a un torneo o a un evento videoludico.', '🎮', 3);
-insert into public."achievement" values('THE STORM THAT IS APPROACHING', 'Bevi una Monster Energy Zero Ultra (e sopravvivi per raccontarlo ai posteri)', '🌌', 1);
-insert into public."achievement" values('NANOMACHINES, SON!', 'Sperimenta la realtà virtuale', '🥽', 1);
+
+-- Sezione viaggio
+INSERT INTO public.achievement VALUES ('FERMAAA A LUCCA COMICS?', 'Chiedere ad un passante in autogrill se va al lucca comics.', '🚏', 1);
+INSERT INTO public.achievement VALUES ('UNA COCHINA BELLA FRESCA', 'Bevi una coca cola durante il viaggio in auto.', '🥤', 1);
+INSERT INTO public.achievement VALUES ('ISTRIANO', 'Non comprare niente all’’autogrill durante una sosta.', '0️⃣', 1);
+INSERT INTO public.achievement VALUES ('SAUDITA', 'Spendi almeno 20€ all’’autogrill durante una sosta.', '🛢️', 1);
+INSERT INTO public.achievement VALUES ('GIORGIO VANNI', 'Canta una sigla di cartoni animati, rigorosamente fine anni 90’’ o inizi 2000.', '🎤', 2);
+INSERT INTO public.achievement VALUES ('MARIO KART', 'Prova l''ebbrezza di farti trasportare da Mario nella sua nuova fiammante vettura.', '🏎️', 1);
+INSERT INTO public.achievement VALUES ('DA ZERO A CENTO', 'Fai la tratta Ronchi-Lucca in meno di 4 ore.', '⏱️', 3);
+INSERT INTO public.achievement VALUES ('IL MASSIMO ESPERTO: EDIZIONE “AUTOSTRADE”', 'Raduna la comitiva (nella macchina) e annuncia, puntando il dito indice in alto, una nozione sulla rete autostradale dell’’Italia.', '📚', 2);
+INSERT INTO public.achievement VALUES ('MAXONE', 'Canta l’intera “La Regina del Celebrità” a memoria.', '🫅', 1);
+INSERT INTO public.achievement VALUES ('STUN GRENADE', 'Scoreggiona rigorosamente silenziosa in macchina .', '💀', 2);
+INSERT INTO public.achievement VALUES ('UNA COCHINA BELLA FRESCA (gone wrong)', 'Rovescia una bevanda in auto.', '🤬', 1);
+
+-- Sezione Fiera
+
+INSERT INTO public.achievement VALUES ('IL RITORNO DEL BROLYCULO', 'Sai già cosa devi fare.', '🍑', 1);
+INSERT INTO public.achievement VALUES ('GOURMET RACE', 'Mangia kebab (o falafel), ramen e pizza in una sola giornata.', '🍜', 2);
+INSERT INTO public.achievement VALUES ('LA CUMMINA', 'Compra e bevi una bevanda discutibile con cummine dentro.', '🧃', 1);
+INSERT INTO public.achievement VALUES ('ULTRA SIDE CHARACTER', 'Ma chi cazzo cosplaya Shino Aburame? Fai una foto con un personaggio super side.', '🥸', 2);
+INSERT INTO public.achievement VALUES ('PARITA’', 'Incontra e fai una foto con un content creator, ma di genere opposto al tuo.', '🏳️‍🌈', 1);
+INSERT INTO public.achievement VALUES ('GAMER ESPERTO', 'Prova almeno 5 videogiochi nello stesso giorno. Punti bonus se partecipi a un torneo o a un evento videoludico.', '🕹️', 3);
+INSERT INTO public.achievement VALUES ('THE STORM THAT IS APPROACHING', 'Bevi una Monster Energy Zero Ultra (e sopravvivi per raccontarlo ai posteri).', '🪫', 1);
+INSERT INTO public.achievement VALUES ('NANOMACHINES, SON!', 'Sperimenta la realtà virtuale.', '🥽', 2);
+INSERT INTO public.achievement VALUES ('SENTIRSI GIOVANI...', 'Porta un cosplay anche improvvisato per un giorno (No, la maglia del Palio di Pieris non è un cosplay).', '🥻', 2);
+INSERT INTO public.achievement VALUES ('LUOGO BUCOLICO - CONQUISTATO', 'Trova l’’accesso al parco della famosa villa visibile dalle Mura in cui non siamo mai riusciti a entrare.', '🥾', 2);
+INSERT INTO public.achievement VALUES ('QUALCUNO L’HA MAI FATTO?', 'Entra nella cattedrale di Lucca. Punti bonus se in cosplay.', '😯', 1);
+INSERT INTO public.achievement VALUES ('LO HANNO FATTO IN MOLTI', 'Prega nella cattedrale di Lucca. Punti bonus se si manifesta un miracolo davanti ai tuoi occhi.', '🛐', 1);
+INSERT INTO public.achievement VALUES ('FANBOY', 'Passa una giornata con del vestiario dedicato a un content creator o a un fumetto/videogioco. Punti bonus se incontri il corrispettivo creator o cosplayer, provandolo.', '🤹', 2);
+INSERT INTO public.achievement VALUES ('TABLETOP MASTA', 'Spendi almeno 55€ in giochi da tavolo o di carte, dal rinomato capannone.', '💸', 3);
+INSERT INTO public.achievement VALUES ('CULTURAL IMPORTER', 'Chiedi una foto a un/a cosplayer ma fallo in un dialetto a scelta.', '💬', 1);
+INSERT INTO public.achievement VALUES ('MIO PADRE? PEFFO', 'Fai cringiare Dario Moccia con un meme stra-morto e provalo con una foto/video.', '😬', 2);
+INSERT INTO public.achievement VALUES ('PERVERTITO', 'Acquista un volume di un hentai. Punti bonus se è un’action figure hentai.', '🌚', 1);
+INSERT INTO public.achievement VALUES ('PEGI 18', 'Fai una foto con un/a cosplayer che presenta almeno il 70% di corpo nudo visibile.', '🔞', 1);
+INSERT INTO public.achievement VALUES ('DIREZIONE: LAGO DURIA', 'Fatti una foto con cosplayer sulla riva del fiume.', '💧', 1);
+INSERT INTO public.achievement VALUES ('HABITUE', 'Trova ristoro e prendi qualcosa da bere a QUEL classico baretto delle Mura.', '☕', 1);
+INSERT INTO public.achievement VALUES ('FANCULO YOUTUBE', 'Fai la foto con un creator che è partito da Twitch, o che comunque non aveva seguito importante su Youtube.', '🎥', 2);
+INSERT INTO public.achievement VALUES ('WEABOO SHIT', 'Compra una cosa che è proprio weaboo shit (action figure, portachiavi anime, manga discutibile, gadget kawaii…).', '🇯🇵', 1);
+INSERT INTO public.achievement VALUES ('SHINZOU WO SASAGEYO', 'Ottieni un’interazione di qualsiasi tipo con quelli dell’esercito. Punti bonus se dura almeno 5 minuti.', '🖖', 1);
+INSERT INTO public.achievement VALUES ('LA CREATIVITA''', 'Spotta la Wednesday.', '🕳️', 1);
+INSERT INTO public.achievement VALUES ('IS THIS A JOJO REFERENCE?', 'Fatti una foto in jojo pose con un cosplay di jojo.', '⭐', 2);
+INSERT INTO public.achievement VALUES ('L''UOMO CHE CAMMINA', 'Fai il classico giro delle mura.', '🚶', 1);
+INSERT INTO public.achievement VALUES ('SIMPATA MAXIMA', 'Trova e fatti una foto con Kurolily.', '😍', 3);
+INSERT INTO public.achievement VALUES ('IL MASSIMO ESPERTO: EDIZIONE “CHRIS WARE”', 'Raduna la comitiva in cerchio e annuncia, puntando il dito indice in alto, una nozione sul Maestro.', '🔍', 3);
+INSERT INTO public.achievement VALUES ('CONTEST: L’INSETTO PIU’ STRANO', 'Fai una foto ad un insetto. Punti in più se è la foto dell’insetto più strano.', '🐛', 1);
+INSERT INTO public.achievement VALUES ('MA TU SEI CARNEADE!!', 'Chiedi ad un personaggio famoso di farti una foto con uno sconosciuto.', '📸', 2);
+INSERT INTO public.achievement VALUES ('YOTOBI: IGNORATO', 'Trova Karim Musa e poi ignoralo. Punti bonus se ciò accade palesemente, ad esempio mentre fai una foto con un creator accanto a lui.', '😶', 3);
+INSERT INTO public.achievement VALUES ('LAZLO VIBES', 'Ottieni una foto con un creator un po'' modificato.', '😵‍💫', 2);
+INSERT INTO public.achievement VALUES ('INNTALATA?', 'Stana e portati a casa un ricordo con un membro di Inntale a scelta.', '🎲', 2);
+INSERT INTO public.achievement VALUES ('DEVOTO', 'Fai una fila per incontrare un personaggio famoso. Punti bonus se ti porti a casa un ricordo da ciò (autografo, bozzetto…).', '🙏', 2);
+INSERT INTO public.achievement VALUES ('CURA PALLIATIVA', 'Caffè corretto al baretto solito.', '🥃', 1);
+INSERT INTO public.achievement VALUES ('SHALOM', 'Gestualità da muro del pianto sull’oratorio in onore dei ricordi passati.', '✡️', 1);
+INSERT INTO public.achievement VALUES ('DEJAVU', 'Arriva a japan town, dal centro di lucca, in meno di 30 minuti.', '🐎', 3);
+INSERT INTO public.achievement VALUES ('IL MASSIMO ESPERTO: EDIZIONE “LA REPUBBLICA DI LUCCA”', 'Raduna la comitiva in cerchio e annuncia, puntando il dito indice in alto, una nozione sulla Repubblica di Lucca.', '📖', 2);
+INSERT INTO public.achievement VALUES ('SAN FRANCESCO', 'Saluta un piccione. Punti in più se ti saluta di rimando.', '🐦', 1);
+INSERT INTO public.achievement VALUES ('WOOOOOOHOAAAAAAAA', 'Senti il tempo volare mentre fumi un sigaro toscano come farebbe Big Boss di Metal Gear Solid.', '🚬', 2);
+INSERT INTO public.achievement VALUES ('WATCHMEN', 'Chiedere che ore sono ad uno sconosciuto. Punti in più se è un Power Ranger rosso.', '⌚', 1);
+INSERT INTO public.achievement VALUES ('STUNNATO', 'Chiedere in fiera dov’è il Lucca Comics.', '🫨', 1);
+INSERT INTO public.achievement VALUES ('EL SOL MAGNA LE ORE', 'Entra a Lucca entro le 7.30 am.', '🌄', 3);
+INSERT INTO public.achievement VALUES ('COSPLAY DI VICTORLAZLO', 'Bevi almeno 7 alcolici in una serata.', '🍷', 3);
+
+-- Sezione Appartamento
+
+INSERT INTO public.achievement VALUES ('NOSTALGICO', 'Dormi in sacco a pelo per tutta la durata del soggiorno. Punti bonus se non usi la doccia.', '🛏️', 3);
+INSERT INTO public.achievement VALUES ('DI CHEESE!', 'Fai una foto a ciascun partecipante mentre sta dormendo.', '😪', 2);
+INSERT INTO public.achievement VALUES ('UNA VOLTA PAR OMO', 'Tocca il pacco di Lorenzo (punti bonus se vieni ritratto come nelle foto dei leader politici che si stringono la mano).', '🫴', 1);
+INSERT INTO public.achievement VALUES ('ORA SONO MORTE, DISTRUTTORE DI MONDI', 'Fai una cagata allucinante nel bagno dell''appartamento ed impestalo per il prossimo utilizzatore.', '💩', 2);
+INSERT INTO public.achievement VALUES ('LA VIA DEL GREMBIULE', 'Cucina o pulisci, insomma renditi utile.', '🧹', 1);
+INSERT INTO public.achievement VALUES ('IL MASSIMO ESPERTO: EDIZIONE “ECONOMIA CASALINGA”', 'Raduna la comitiva in cerchio e annuncia, puntando il dito indice in alto, un consiglio su come risparmiare sulla gestione quotidiana della casa.', '📕', 2);
+INSERT INTO public.achievement VALUES ('LA FATINA DEI DENTI', 'Metti una monetina sotto il cuscino del bambino fortunato.', '🪙', 2);
+INSERT INTO public.achievement VALUES ('MOMENTO FRATERNO', 'Urla “bruh” a squarciagola nel cuore della notte. Punti in più se si sveglia qualcuno.', '📣', 2);
+INSERT INTO public.achievement VALUES ('SKIBIDI TOILET', 'Neutralizza la skibidi toilet.', '🛁', 2);
